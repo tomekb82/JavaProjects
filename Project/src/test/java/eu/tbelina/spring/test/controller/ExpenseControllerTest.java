afe6fc9d36e6dcc050593be0edfe33bdefc0b98c
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-09 00:32:55 +0200

    Dodac testy jednostkowe kontrolera.

---
+++ Project/src/test/java/eu/tbelina/spring/test/controller/ExpenseControllerTest.java
@@ -0,0 +1,46 @@
+package eu.tbelina.spring.test.controller;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+import com.mchange.util.AssertException;
+
+import eu.tbelina.spring.controller.ExpenseController;
+import eu.tbelina.spring.model.Expense;
+import eu.tbelina.spring.service.impl.ExpenseService;
+
+public class ExpenseControllerTest {
+	
+	@Test
+	public void shouldDisplayRecentExpenses(){
+				
+		List<Expense> expectedExpenses = Arrays.asList(new Expense(), new Expense(), new Expense());
+		
+		// Mock service implementation
+		ExpenseService expenseService = mock(ExpenseService.class);
+		
+		when(expenseService.getExpenses()).thenReturn(expectedExpenses);
+		
+		// Create controller
+		ExpenseController controller = new ExpenseController(expenseService);
+		
+		// Run operating method
+		HashMap<String, Object> model = new HashMap<String, Object>();
+		String viewName = controller.showAllExpenses(model);
+		
+		assertEquals("expenses", viewName);
+		
+		// Check result
+		assertSame(expectedExpenses, model.get("expenses"));
+		verify(expenseService).getExpenses();
+		
+	}
+
+}
