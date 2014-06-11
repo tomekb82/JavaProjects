858b74e51bc7781dd61b3e56cdd26b1be33ad910
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-11 21:29:35 +0200

    Dodac funkcje hashcode() i equals()

--- Project/src/main/java/eu/tbelina/spring/model/Expense.java
+++ Project/src/main/java/eu/tbelina/spring/model/Expense.java
@@ -86,4 +86,39 @@ public class Expense implements Serializable{
 				+ ", quantity=" + quantity + ", date=" + date + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + quantity;
+		result = prime * result + Float.floatToIntBits(value);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Expense other = (Expense) obj;	
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;		
+		if (quantity != other.quantity)
+			return false;	
+		if (Float.floatToIntBits(value) != Float.floatToIntBits(other.value))
+			return false;
+		return true;
+	}
+	
+	
+
 }
