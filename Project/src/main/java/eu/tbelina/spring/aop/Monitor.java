8d1466944c7fa7925407f8a3e9566665bd6c9c97
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-11 21:20:14 +0200

    Zakomentowac adnotacje. Dodac aspekt AROUND

--- Project/src/main/java/eu/tbelina/spring/aop/Monitor.java
+++ Project/src/main/java/eu/tbelina/spring/aop/Monitor.java
@@ -1,16 +1,17 @@
 package eu.tbelina.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect
+//@Aspect
 public class Monitor {
 	
-	@Before("execution(*eu.tbelina.spring.service.impl.ExpenseService.addExpense(..))")
+	//@Before("execution(*eu.tbelina.spring.service.impl.ExpenseService.addExpense(..))")
 	public void logBefore(JoinPoint joinPoint) {
  
 		System.out.println("logBefore() is running!");
@@ -19,24 +20,40 @@ public class Monitor {
 	}
 	
 	//@Pointcut("execution(*eu.tbelina.spring.service.impl.ExpenseService.getExpenses(..))")
-	@Pointcut("execution(**.getExpenses(..))")
+	//@Pointcut("execution(**.getExpenses(..))")
 	public void calcultation(){
 		
 	}
 	
-	@Before("calcultation()")
+	//@Before("calcultation()")
 	public void beforeCalculation(){
-		System.out.println("Preparing to calculation.");
+		System.out.println("Preparing to calculation !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.");
 	}
 	
-	@AfterReturning("calcultation()")
+	//@AfterReturning("calcultation()")
 	public void afterCalculation(){
 		System.out.println("After calculation.");
 	}
 	
-	@AfterThrowing("calcultation()")
+	//@AfterThrowing("calcultation()")
 	public void incorrectCalculation(){
 		System.out.println("Incorecct calculation.");
+	}
+	//@Around("calcultation()")
+	public void calculatePerformance(ProceedingJoinPoint joinPoint){
+		
+		try{
+			System.out.println("Start the performance test...");
+			long start = System.currentTimeMillis();
+			
+			joinPoint.proceed();
+			
+			long end = System.currentTimeMillis();
+			System.out.println(" Results of the performance test:time = " + (end-start) + "ms");
+		}catch(Throwable t){
+			System.out.println("Performance test failed.");
+		}
+		
 	}
 
 }
