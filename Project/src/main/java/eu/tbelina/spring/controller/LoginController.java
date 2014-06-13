4fe661cbdb64211acfb48e045f78073d636d1a8f
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-13 13:23:25 +0200

    Dodac kontroler do obslugi formularza logowania

---
+++ Project/src/main/java/eu/tbelina/spring/controller/LoginController.java
@@ -0,0 +1,36 @@
+package eu.tbelina.spring.controller;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import eu.tbelina.spring.model.UserForm;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String viewLogin(Map<String, Object> model) {
+        UserForm user = new UserForm();
+        model.put("userForm", user);
+        return "LoginForm";
+    }
+ 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String doLogin(@Valid @ModelAttribute("userForm") UserForm userForm,
+            BindingResult result, Map<String, Object> model) {
+ 
+        if (result.hasErrors()) {
+            return "LoginForm";
+        }
+ 
+        return "LoginSuccess";
+    }
+    
+}
