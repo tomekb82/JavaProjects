bacbb362dca14ba46cdfbce0f3328ab6391b841d
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-15 22:47:57 +0200

    Dodac plik z domyslna konfiguracja bazy danych (dodanie nowych danych).

---
+++ Project/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO expenses(id, name, value, quantity, date) VALUES(1001, 'marta', 32, 34, now());
+INSERT INTO expenses(id, name, value, quantity, date) VALUES(1002, 'jola', 46, 87, now());
+INSERT INTO expenses(id, name, value, quantity, date) VALUES(1003, 'helena', 12, 12, now());
+INSERT INTO expenses(id, name, value, quantity, date) VALUES(1004, 'krystyna', 76, 76, now());
