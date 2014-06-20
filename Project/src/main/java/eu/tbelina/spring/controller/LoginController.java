e14c299edf345b0bfe282329cb23462221d21b22
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-20 23:38:03 +0200

    Poprawic sciezki.

--- Project/src/main/java/eu/tbelina/spring/controller/LoginController.java
+++ Project/src/main/java/eu/tbelina/spring/controller/LoginController.java
@@ -19,7 +19,7 @@ public class LoginController {
     public String viewLogin(Map<String, Object> model) {
         UserForm user = new UserForm();
         model.put("userForm", user);
-        return "/jsp/LoginForm";
+        return "LoginForm";
     }
  
     @RequestMapping(value = "/login2", method = RequestMethod.POST)
@@ -27,10 +27,10 @@ public class LoginController {
             BindingResult result, Map<String, Object> model) {
  
         if (result.hasErrors()) {
-            return "/jsp/LoginForm";
+            return "LoginForm";
         }
  
-        return "/jsp/LoginSuccess";
+        return "LoginSuccess";
     }
     
 }
