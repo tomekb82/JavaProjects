d8d1feca0c3674207a6847bf73e1f0e89bf15e89
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-20 21:52:16 +0200

    Usunac niewykorzystywane importy.

--- Project/src/main/java/eu/tbelina/spring/security/SecurityLoginController.java
+++ Project/src/main/java/eu/tbelina/spring/security/SecurityLoginController.java
@@ -1,18 +1,10 @@
 package eu.tbelina.spring.security;
 
 import java.security.Principal;
-import java.util.Map;
-
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import eu.tbelina.spring.model.UserForm;
 
 @Controller
 public class SecurityLoginController {
