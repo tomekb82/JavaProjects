81cbcff86943d8f1fabf1a3fe38491dc2c0b4420
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-23 00:21:08 +0200

    Zmienic referencje do modulu.

--- Project/src/main/webapp/js/directives/focus.js
+++ Project/src/main/webapp/js/directives/focus.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('ngdemo.directives')
-.directive('focus', function() {
+var directive = angular.module('angularspring.directives', []);
+
+directive.directive('ngbkFocus', function() {
 	return {
 		link: function(scope, element, attrs) {
 			element[0].focus();
