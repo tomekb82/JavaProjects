fb624685e208bf3799abe0ef82eddc20a68bb1da
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-23 18:38:18 +0200

    Usunac niewykorzystywane serwisy przeniesione do expenses.js

--- Project/src/main/webapp/js/services/Expense.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var services = angular.module('angularspring.services', ['ngResource']);
-
-services.factory('Expense', ['$resource', function($resource) {
-        return $resource('mvc/rest_expenses/json2/:name', {name: '@name'});
-}]);
