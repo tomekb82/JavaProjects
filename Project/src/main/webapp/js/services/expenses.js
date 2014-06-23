592c5b956f52a1aa8a108230613827f911691fa5
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-23 18:36:47 +0200

    Dodac serwisy do pobierania wydatkow.

--- Project/src/main/webapp/js/services/expenses.js
+++ Project/src/main/webapp/js/services/expenses.js
@@ -4,8 +4,9 @@
 
 var services = angular.module('angularspring.services', ['ngResource']);
 
-	services.factory('ExpenseFactory', function ($resource) {
-    	return $resource('mvc/rest_expenses/json/:name', {name: '@name'}, {
+// INFO: not used
+services.factory('ExpenseFactory', function ($resource) {
+	return $resource('mvc/rest_expenses/json/:name', {name: '@name'}, {
         	query: {
             	method: 'GET',
             	params: {},
@@ -17,40 +18,41 @@ var services = angular.module('angularspring.services', ['ngResource']);
     			isArray: false
     	  }
         });
-    });
+});
 	
-	services.factory('Expense', ['$resource', function($resource) {
-	        return $resource('mvc/rest_expenses/json/:name', {name: '@name'});
-	}]);
-	
-	services.factory('MultiExpenseLoader', ['expenses', '$q', function(expenses, $q) {
-		return function() {
-			var delay = $q.defer();
-	        	Expense.query(
-	        			// on success
-	                    function(expenses) {
-	                    	delay.resolve(expenses);
-	                    }, 
-	                    // on error
-	                    function() {
-	                    	delay.reject('Unable to fetch expenses');
-	                    }
-	             );
-	        return delay.promise;
-	   };
-	}]);
-	
-	services.factory('ExpenseLoader', ['Expense', '$route', '$q',function(Expense, $route, $q) {
-		return function() {
-			var delay = $q.defer();
+services.factory('Expense', ['$resource', function($resource) {
+        return $resource('mvc/rest_expenses/json/:name', {name: '@name'});
+}]);
+
+
+services.factory('MultiExpenseLoader', ['Expense', '$q', function(Expense, $q) {
+	return function() {
+		var delay = $q.defer();
+        	Expense.query(
+        			// on success
+                    function(expenses) {
+                    	delay.resolve(expenses);
+                    }, 
+                    // on error
+                    function() {
+                    	delay.reject('Unable to fetch expenses');
+                    }
+             );
+        return delay.promise;
+   };
+}]);
+
+services.factory('ExpenseLoader', ['Expense', '$route', '$q',function(Expense, $route, $q) {
+	return function() {
+	var delay = $q.defer();
 	    
-			Expense.get({name: $route.current.params.name}, function(expense) {
-				delay.resolve(expense);
-			}, function() {
-				delay.reject('Unable to fetch expense ', $route.current.params.name);
-			});
+	Expense.get({name: $route.current.params.name}, function(expense) {
+	delay.resolve(expense);
+	}, function() {
+	delay.reject('Unable to fetch expense ', $route.current.params.name);
+	});
 	    
-			return delay.promise;
+	return delay.promise;
 	  };
 	}]);
 	
