e9a8fadfefe12831a99704e3ce86e8f238a371c6
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-28 10:59:18 +0200

    Dodac szablon projektu klienta EJB.

---
+++ JBOSS/MavenProject/ticket-agency-ejb-client/src/main/java/eu/tbelina/jboss/ticket_agency_ejb_client/App.java
@@ -0,0 +1,13 @@
+package eu.tbelina.jboss.ticket_agency_ejb_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
