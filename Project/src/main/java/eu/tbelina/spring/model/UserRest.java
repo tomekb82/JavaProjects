0734d3b82665188443abd447b70eb91446bd452b
Tomasz Belina <tomasz.belina@qualent.eu>
2014-06-29 14:28:05 +0200

    Uzupelnic adnotacje encji.

--- Project/src/main/java/eu/tbelina/spring/model/UserRest.java
+++ Project/src/main/java/eu/tbelina/spring/model/UserRest.java
@@ -1,42 +1,52 @@
 package eu.tbelina.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+@Entity
+@Table(name="user_rest")
 public class UserRest {
 
-	 @Override
-	public String toString() {
-		return "UserRest [id=" + id + ", name=" + name + ", age=" + age + "]";
-	}
-	public static final int INVALID_ID = -1;
-	 private int id = INVALID_ID;
 	 
-	 @Id
-		@GenericGenerator(name="hilo-strategy", strategy = "hilo")
-		@GeneratedValue(generator = "hilo-strategy")
-	 public int getId() {
+	public static final int INVALID_ID = -1;
+	private int id = INVALID_ID;
+	private String name;
+	private int age;
+  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="USER_ID", unique = true, nullable = false)
+	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	@Column(name="USER_NAME", unique = true, nullable = false)
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Column(name="USER_AGE", unique = true, nullable = false)
 	public int getAge() {
 		return age;
 	}
 	public void setAge(int age) {
 		this.age = age;
 	}
-	private String name;
-	 private int age;
-
-	 
+	
+	@Override
+	public String toString() {
+		return "UserRest [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
 }
