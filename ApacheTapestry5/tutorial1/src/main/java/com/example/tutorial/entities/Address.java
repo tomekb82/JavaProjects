cd7dc8f0c9eb5225536915ac94f350e2974374f0
Tomasz Belina <tomasz.belina@qualent.eu>
2014-12-07 10:17:44 +0100

    Dodaj wsparcie dla hibernate

--- ApacheTapestry5/tutorial1/src/main/java/com/example/tutorial/entities/Address.java
+++ ApacheTapestry5/tutorial1/src/main/java/com/example/tutorial/entities/Address.java
@@ -1,28 +1,45 @@
 package com.example.tutorial.entities;
  
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.apache.tapestry5.beaneditor.NonVisual;
 import org.apache.tapestry5.beaneditor.Validate;
 
 import com.example.tutorial.data.Honorific;
  
+@Entity
 public class Address
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonVisual //indicates a field, such as a primary key, that should not be made visible to the user.
+	public Long id;
+	
     public Honorific honorific;
+    
     @Validate("required")
     public String firstName;
+   
     @Validate("required")
     public String lastName;
-    @Validate("required")
+   
     public String street1;
-    @Validate("required")
+   
     public String street2;
+   
     @Validate("required")
     public String city;
+   
     @Validate("required")
     public String state;
+   
     @Validate("required,regexp")
     public String zip;
-    @Validate("required")
+   
     public String email;
-    @Validate("required")
+   
     public String phone;
 }
--- ApacheTapestry5/tutorial1/src/main/java/com/example/tutorial/pages/address/CreateAddress.java
+++ ApacheTapestry5/tutorial1/src/main/java/com/example/tutorial/pages/address/CreateAddress.java
@@ -1,11 +1,30 @@
 package com.example.tutorial.pages.address;
 
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
 
 import com.example.tutorial.entities.Address;
+import com.example.tutorial.pages.AddressBook;
 
 public class CreateAddress {
 
 	@Property
-	private Address address;
+    private Address address;
+ 
+    @Inject//inject a service into the annotated field;
+    private Session session;
+ 
+    @InjectPage
+    private AddressBook addressBook;
+ 
+    @CommitAfter // can be applied to any component method; if the method completes normally, the transaction will be committed 
+    Object onSuccess()
+    {
+        session.persist(address);
+ 
+        return addressBook;
+    }
 }
