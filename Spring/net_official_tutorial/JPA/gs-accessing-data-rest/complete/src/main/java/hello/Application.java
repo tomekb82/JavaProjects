5df32288e640a06df7912a92883b71ad4fb2996a
Tomasz Belina <tomasz.belina@qualent.eu>
2014-12-13 12:32:22 +0100

    JPA i odczyt rest danych jpa na bazie

---
+++ Spring/net_official_tutorial/JPA/gs-accessing-data-rest/complete/src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
