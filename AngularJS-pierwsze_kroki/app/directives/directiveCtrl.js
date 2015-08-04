0ef8862fbe029f76b4a75a52e1d074af57994ac0
Tomasz Belina <tomasz.belina@qualent.eu>
2015-08-04 22:30:40 +0200

    Aktualizacja

--- AngularJS-pierwsze_kroki/app/directives/directiveCtrl.js
+++ AngularJS-pierwsze_kroki/app/directives/directiveCtrl.js
@@ -19,30 +19,30 @@
 		$scope.change = function () {
 			console.log($scope.testModel);
 		};
-		//ngClass, ngRepeat
-$scope.sorting = '-metres';
-$scope.mountainsList = [
-{ mountain: "Mount Everest", metres: 8850 },
-{ mountain: "K2", metres: 8611 },
-{ mountain: "Kangczendzonga", metres: 8598 },
-{ mountain: "Lhotse", metres: 8501 },
-{ mountain: "Makalu", metres: 8463 },
-{ mountain: "Cho Oyu", metres: 8201 }];
-var drive = function (source, target) {
-var t = $scope.mountainsList[target];
-$scope.mountainsList[target] = $scope.mountainsList[source];
-$scope.mountainsList[source] = t;
-};
-$scope.up = function (index) {
-drive(index, index - 1);
-};
-$scope.down = function (index) {
-drive(index, index + 1);
-};
-$scope.saveChanges = function (index, mountain, metres) {
-$scope.mountainsList[index]= { 'mountain': mountain, 'metres':
-metres };
-};
+		//ngClass, ngRepeat, ngClick
+		$scope.sorting = '-metres';
+		$scope.result = null;
+		$scope.mountainsList = [
+			{ mountain: "Mount Everest", metres: 8850, execute: function (height){ $scope.result= height + ', function 1'; } },
+			{ mountain: "K2", metres: 8611, execute: function (height) { $scope.result = height + ', function 2'; } },
+			{ mountain: "Kangczendzonga", metres: 8598, execute: function (height) { $scope.result = height + ', function 3'; } },
+			{ mountain: "Lhotse", metres: 8501, execute: function (height) { $scope.result = height + ', function 4'; } },
+			{ mountain: "Makalu", metres: 8463, execute: function (height) { $scope.result = height + ', function 5'; } },
+			{ mountain: "Cho Oyu", metres: 8201, execute: function (height) { $scope.result = height + ', function 6'; } }];
+		var drive = function (source, target) {
+			var t = $scope.mountainsList[target];
+			$scope.mountainsList[target] = $scope.mountainsList[source];
+			$scope.mountainsList[source] = t;
+		};
+		$scope.up = function (index) {
+			drive(index, index - 1);
+		};
+		$scope.down = function (index) {
+			drive(index, index + 1);
+		};
+		$scope.saveChanges = function (index, mountain, metres) {
+			$scope.mountainsList[index]= { 'mountain': mountain, 'metres':metres };
+		};
 	}
 
 	function appConfiguration($stateProvider) {
