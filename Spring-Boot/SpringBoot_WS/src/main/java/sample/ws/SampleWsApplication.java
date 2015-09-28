138c55439376e04e5385d6f2833765063e340612
Tomasz Belina <tomasz.belina@qualent.eu>
2015-09-29 00:07:58 +0200

    Dodaj projekt WS z wykorzystaniem spring boot

---
+++ Spring-Boot/SpringBoot_WS/src/main/java/sample/ws/SampleWsApplication.java
@@ -0,0 +1,14 @@
+
+package sample.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWsApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleWsApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot/SpringBoot_WS/src/main/java/sample/ws/service/HumanResourceService.java
@@ -0,0 +1,10 @@
+
+package sample.ws.service;
+
+import java.util.Date;
+
+public interface HumanResourceService {
+
+    void bookHoliday(Date startDate, Date endDate, String name);
+
+}
